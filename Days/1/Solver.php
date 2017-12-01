43f24aeb5bbc2ca106e93c53be3a5d793415bc8e
Wouter Goyen <wouter@xesxen.nl>
2017-12-01 23:48:19 +0100

    Whoops, forgot part 2

--- Days/1/Solver.php
+++ Days/1/Solver.php
@@ -1,11 +1,12 @@
 <?php
 
-if($argc != 2) {
-    echo "Please supply the input as an argument";
+if($argc != 3) {
+    echo "Please supply the input and part as an argument";
     exit(1);
 }
 
 $input = $argv[1];
+$mode = $argv[2];
 $length = strlen($input);
 $points = 0;
 
@@ -15,12 +16,26 @@ if(!preg_match('/^[\d]+$/', $input)) {
 }
 
 $input = str_split($input);
-$input[] = $input[0]; // Cyclic list
 
-foreach($input as $idx => $val) {
-       if($idx < $length && $input[$idx + 1] == $val) {
-           $points += intval($val);
-       }
+switch($mode) {
+    case 1:
+        foreach($input as $idx => $val) {
+            $next_idx = ($idx + 1) % $length;
+            if($idx < $length && $input[$next_idx] == $val) {
+                $points += intval($val);
+            }
+        }
+
+        break;
+    case 2:
+        foreach($input as $idx => $val) {
+            $next_idx = ($idx + $length / 2) % $length;
+            if($idx < $length && $input[$next_idx] == $val) {
+                $points += intval($val);
+            }
+        }
+        break;
 }
+
 
 echo "Done, " . $points;
